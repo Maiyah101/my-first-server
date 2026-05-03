48473136d7f8f189e5bfd484ab28f0f62519906f
Maiyah101 <sumbaksh@gmail.com>
2026-05-03 13:39:21 -0400

    my first server
    spring server flowers with pink background

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-// Your server code goes here. Host your server on localhost:3000
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/styles.css") {
+    fs.readFile(path.join(__dirname, "styles.css"), (err, data) => {
+      res.writeHead(200, { "Content-Type": "text/css" });
+      res.end(data);
+    });
+    return;
+  }
+
+  fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Server Error");
+      return;
+    }
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server running on http://localhost:" + port);
+});
